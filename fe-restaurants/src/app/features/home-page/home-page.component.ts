a48cf1eee4cad86c7dbb9f49cdcc48d7084b4267
t.bricchi <thomas.bricchi_ext@alten.it>
2020-12-03 09:48:24 +0100

    show and hide for press same button

--- fe-restaurants/src/app/features/home-page/home-page.component.ts
+++ fe-restaurants/src/app/features/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Restaurant} from "./model/restaurant.model";
 import {RestaurantService} from "../../services/restaurant.service";
-import {Observable} from "rxjs";
+import {EMPTY, Observable} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
@@ -9,6 +9,8 @@ import {Observable} from "rxjs";
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  lastRestaurantSelected: number;
+
   ngOnInit(): void {
     this.restaurants$ = this.restaurantService.loadAll();
   }
@@ -21,7 +23,14 @@ export class HomePageComponent implements OnInit {
 
 
   selectRestaurant(restaurantSelected: Restaurant) {
-    this.restaurantSelected$ = this.restaurantService.getOne(restaurantSelected.id);
+    if (this.lastRestaurantSelected !== restaurantSelected.id) {
+      this.restaurantSelected$ = this.restaurantService.getOne(restaurantSelected.id);
+      this.lastRestaurantSelected = restaurantSelected.id;
+    } else {
+      this.restaurantSelected$ = EMPTY;
+      this.lastRestaurantSelected = null;
+    }
+
   }
 
 }
