7e5a92537d63bdeab87115184c4de29b1c55ed67
thomasbricchi <dev@wideside.ch>
2020-12-04 15:41:19 +0100

    test cypress

--- fe-restaurants/cypress/integration/home-page.spec.js
+++ fe-restaurants/cypress/integration/home-page.spec.js
@@ -1,32 +1,49 @@
-import {createRestaurant, createRestaurantWithDetails} from "../../src/test-utils/test-utils";
-
 describe('Login Feature', () => {
 
-  // it('should be enter in home page and see the list', () => {
-  //
-  //   cy.visit('/');
-  //
-  //   cy.contains('La Pasta');
-  //   cy.contains('Niederdorfstrasse 80, 8001 Zürich, Schweiz');
-  //   cy.contains('Type of cuisine: Italian');
-  //
-  //   cy.contains('Heugümper');
-  //   cy.contains('Waaggasse 4, 8001 Zürich, Schweiz');
-  //   cy.contains('Type of cuisine: Fusion');
-  //
-  //   cy.contains('Mesa');
-  //   cy.contains('Weinbergstrasse 75, 8006 Zürich, Schweiz');
-  //   cy.contains('Type of cuisine: European');
-  //
-  // });
-
-  it('should show data of first restaurants at start', () => {
+  it('should be enter in home page and see the list', () => {
 
     cy.visit('/');
 
-    cy.getByDataCySel("details-1");
-    cy.getByDataCySel("details-2").should('not.exist');
+    cy.contains('La Pasta');
+    cy.contains('Niederdorfstrasse 80, 8001 Zürich, Schweiz');
+    cy.contains('Type of cuisine: Italian');
+
+    cy.contains('Heugümper');
+    cy.contains('Waaggasse 4, 8001 Zürich, Schweiz');
+    cy.contains('Type of cuisine: Fusion');
+
+    cy.contains('Mesa');
+    cy.contains('Weinbergstrasse 75, 8006 Zürich, Schweiz');
+    cy.contains('Type of cuisine: European');
+
   });
 
+  it('should show details of first restaurants at start', () => {
+
+    cy.visit('/');
+
+    cy.get("#details-1");
+    cy.get("#details-2").should('not.exist');
+  });
+
+  it('should close details when click the same restaurant in view details', () => {
+
+    cy.visit('/');
+
+    cy.get('#list-1').click();
+    cy.get("#details-1").should('not.exist');
+  });
+
+
+  it('should show details of restaurant when click on a card', () => {
+
+    cy.visit('/');
+
+    cy.contains('Heugümper').click();
+
+
+    cy.get("#details-2");
+    cy.get("#details-1").should('not.exist');
+  });
 
 });
