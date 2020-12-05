acb679f8eb65004374a695b8d9a36dacc33d1826
thomasbricchi <dev@wideside.ch>
2020-12-05 17:01:44 +0100

    healthCheck

--- restaurants/src/main/java/com/thomas/restaurants/RestaurantsApplication.java
+++ restaurants/src/main/java/com/thomas/restaurants/RestaurantsApplication.java
@@ -2,12 +2,23 @@ package com.thomas.restaurants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestaurantsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsApplication.class, args);
+    }
+
+    @GetMapping("/api/health-check")
+    public ResponseEntity<Void> getAllRestaurants() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
