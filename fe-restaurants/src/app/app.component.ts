9044ffc5ef23c5ab0b84e0b0e476d684d380d540
thomasbricchi <dev@wideside.ch>
2020-12-05 22:44:04 +0100

    refactor health-check

--- fe-restaurants/src/app/app.component.ts
+++ fe-restaurants/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HealthCheckService } from './features/health-check.service';
 import { Observable } from 'rxjs';
-import { catchError, delay, map, retryWhen, take, tap } from 'rxjs/operators';
-import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -13,22 +11,11 @@ export class AppComponent implements OnInit {
 
   showApp$: Observable<boolean>;
 
-  constructor(private healthService: HealthCheckService, private spinner: NgxSpinnerService) {
+  constructor(private healthService: HealthCheckService) {
   }
 
   ngOnInit(): void {
-    this.spinner.show();
-    this.showApp$ = this.healthService.healthCheck().pipe(
-      retryWhen((err) => err.pipe(delay(3000), take(10))),
-      tap(() => this.spinner.hide()),
-      map(() => true),
-      catchError((e) => {
-          alert('Be is still down');
-          throw(e);
-        }
-      ))
-    ;
-
+    this.showApp$ = this.healthService.beIsUp();
   }
 
 }
--- fe-restaurants/src/app/features/health-check.service.ts
+++ fe-restaurants/src/app/features/health-check.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { catchError, delay, map, retryWhen, take, tap } from 'rxjs/operators';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +11,24 @@ export class HealthCheckService {
 
   private resourceUrl = '/api/health-check';
 
-  constructor(private readonly http: HttpClient) {
+  constructor(private readonly http: HttpClient, private spinner: NgxSpinnerService) {
   }
 
   healthCheck(): Observable<void> {
     return this.http.get<void>(this.resourceUrl);
   }
+
+  beIsUp(): Observable<boolean> {
+    this.spinner.show();
+    return this.healthCheck().pipe(
+      retryWhen((err) => err.pipe(delay(3000), take(20))),
+      tap(() => this.spinner.hide()),
+      map(() => true),
+      catchError((e) => {
+        alert('Be is still down');
+        this.spinner.hide();
+        throw(e);
+      }));
+  }
+
 }
